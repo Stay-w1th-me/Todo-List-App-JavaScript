a6cc9945c6a7bceecdd81e3fa36dfd36388cfc94
Stay-w1th-me <163579343+Stay-w1th-me@users.noreply.github.com>
2026-09-19 22:10:43 +0300

    Add task persistence with localStorage

--- script.js
+++ script.js
@@ -2,6 +2,54 @@ const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 
+function saveTasks() {
+    const tasks =[];
+
+    document.querySelectorAll("#taskList li").forEach(function (listItem) {
+        tasks.push({
+            text: listItem.querySelector("span").textContent,
+            completed: listItem.classList.contains("completed")
+        });
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const savedTasks =JSON.parse(localStorage.getItem("tasks")) || [];
+
+    savedTasks.forEach(function (task) {
+        const listItem = document.createElement("li");
+
+        const taskSpan = document.createElement("span");
+        taskSpan.textContent = task.text;
+
+        if (task.completed) {
+            listItem.classList.add("completed");
+        }
+
+        taskSpan.addEventListener("click", function () {
+            listItem.classList.toggle("completed");
+            saveTasks();
+        });
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Удалить";
+
+        deleteButton.addEventListener("click", function () {
+            listItem.remove();
+            saveTasks();
+        });
+
+        listItem.appendChild(taskSpan);
+        listItem.appendChild(deleteButton);
+        
+        taskList.appendChild(listItem);
+    });
+}
+
+loadTasks();
+
 addTaskButton.addEventListener("click", function (){
     const taskText = taskInput.value.trim();
 
@@ -15,7 +63,8 @@ addTaskButton.addEventListener("click", function (){
     taskSpan.textContent = taskText;
 
     taskSpan.addEventListener('click',function (){
-        listItem.classList.toggle("completed")
+        listItem.classList.toggle("completed");
+        saveTasks();
     })
 
     const deleteButton = document.createElement("button");
@@ -23,10 +72,18 @@ addTaskButton.addEventListener("click", function (){
 
     deleteButton.addEventListener("click", function (){
          listItem.remove();
+         saveTasks();
     });
     
     listItem.appendChild(taskSpan);
     listItem.appendChild(deleteButton);
     
     taskList.appendChild(listItem);
+
+    saveTasks();
+
+    taskInput.value = "";
 }); 
+
+
+
