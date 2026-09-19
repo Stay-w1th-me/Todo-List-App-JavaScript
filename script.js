2e945a1141c28f5f70b4e0c5483f52b7cf7878f5
Stay-w1th-me <163579343+Stay-w1th-me@users.noreply.github.com>
2026-09-19 20:50:41 +0300

    Add task deletion

--- script.js
+++ script.js
@@ -10,9 +10,19 @@ addTaskButton.addEventListener("click", function (){
     }
 
     const listItem = document.createElement("li");
-    listItem.textContent = taskText;
+
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Удалить"
+
+    deleteButton.addEventListener("click", function (){
+         listItem.remove();
+    });
+    
+    listItem.appendChild(taskSpan);
+    listItem.appendChild(deleteButton);
     
     taskList.appendChild(listItem);
-
-    taskInput.value = "";
 }); 
